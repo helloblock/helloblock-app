beb951dc1c8ecee1b42520a36c0c384c43dc4760
Scott Li <scottli0101@gmail.com>
2014-01-12 15:24:45 -0800

    tx

---
+++ app/assets/javascripts/controllers/blockExplorer/transactionsCtrl.js
@@ -0,0 +1,5 @@
+hbApp.controller( "blockExplorer/transactionsCtrl", function( $scope ) {
+
+	// $scope.tester = "fuarkkkk"
+
+} )
--- app/assets/javascripts/hbApp.js
+++ app/assets/javascripts/hbApp.js
@@ -1,70 +1,70 @@
 'use strict';
 
 var hbApp = angular.module( 'hbApp', [
-	'ngRoute',
-	'ngResource',
-	'ui.bootstrap',
-	'ui.select2'
+  'ngRoute',
+  'ngResource',
+  'ui.bootstrap',
+  'ui.select2'
 ] )
 
 hbApp.config( function( $locationProvider ) {
-	$locationProvider.html5Mode( true );
+  $locationProvider.html5Mode( true );
 } )
 
 hbApp.run( function( $rootScope, $location ) {
-	$rootScope.global = {
-		isOnLink: function( path ) {
-			return path === $location.path()
-		},
-		language: "curl",
-		setLanguage: function( name ) {
-			$rootScope.global.language = name;
-		}
-	}
+  $rootScope.global = {
+    isOnLink: function( path ) {
+      return path === $location.path()
+    },
+    language: "curl",
+    setLanguage: function( name ) {
+      $rootScope.global.language = name;
+    }
+  }
 } )
 
 hbApp.config( function( $routeProvider ) {
 
-	$routeProvider.when( "/", {
-		templateUrl: "/templates/landing.html"
-	} )
+  $routeProvider.when( "/", {
+    templateUrl: "/templates/landing.html"
+  } )
 
-	$routeProvider.when( "/docs", {
-		templateUrl: "/templates/docs.html",
-		controller: "docsCtrl"
-	} )
+  $routeProvider.when( "/docs", {
+    templateUrl: "/templates/docs.html",
+    controller: "docsCtrl"
+  } )
 
-	$routeProvider.when( "/explorer", {
-		templateUrl: "/templates/explorer.html",
-		controller: "explorerCtrl"
-	} )
+  $routeProvider.when( "/explorer", {
+    templateUrl: "/templates/explorer.html",
+    controller: "explorerCtrl"
+  } )
 
-	Route.namespace( "/testnet", "blockExplorer", function( url, name ) {
-		$routeProvider.when( url, {
-			templateUrl: "/templates/" + name + "/home.html",
-			controller: name + "/homeCtrl"
-		} )
+  Route.namespace( "/testnet", "blockExplorer", function( url, name ) {
+    $routeProvider.when( url, {
+      templateUrl: "/templates/" + name + "/home.html",
+      controller: name + "/homeCtrl"
+    } )
 
-		$routeProvider.when( url + "/addresses", {
-			templateUrl: "/templates/" + name + "/addresses.html",
-			controller: name + "/addressesCtrl"
-		} )
+    $routeProvider.when( url + "/addresses", {
+      templateUrl: "/templates/" + name + "/addresses.html",
+      controller: name + "/addressesCtrl"
+    } )
 
-		$routeProvider.when( url + "/transactions", {
-			templateUrl: "/templates/" + name + "/transactions.html",
-			controller: name + "/transactionsCtrl"
-		} )
-	} )
+    $routeProvider.when( url + "/transactions", {
+      templateUrl: "/templates/" + name + "/transactions.html",
+      controller: name + "/transactionsCtrl"
+    } )
+  } )
 
-	$routeProvider.otherwise( {
-		redirectTo: '/'
-	} );
+  $routeProvider.otherwise( {
+    redirectTo: '/'
+  } );
 
 } )
 
 var Route = {
-	namespace: function( url, name, callback ) {
-		callback( url, name )
-	}
+  namespace: function( url, name, callback ) {
+    callback( url, name )
+  }
 
 }
