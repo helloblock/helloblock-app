79887ac58d704fb09f90d9262fd351b7fc70e3f6
Scott Li <scottli0101@gmail.com>
2014-01-12 18:04:27 -0800

    font fixes

--- app/assets/javascripts/directives/subnav.js
+++ app/assets/javascripts/directives/subnav.js
@@ -2,6 +2,9 @@ hbApp.directive( 'subnav', function() {
 
 	return {
 		restrict: "E",
+		scope: {
+			selected: "="
+		},
 		templateUrl: "/templates/components/_subnav.html",
 		transclude: true
 	}
