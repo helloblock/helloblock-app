5316585a483adc6ad586a9a36fa7dd136e34b874
Scott Li <scottli0101@gmail.com>
2014-01-24 22:51:45 -0800

    propagate page

--- app/assets/javascripts/controllers/blockExplorer/propagateCtrl.js
+++ app/assets/javascripts/controllers/blockExplorer/propagateCtrl.js
@@ -4,4 +4,12 @@ hbApp.controller( "blockExplorer/propagateCtrl", function( $scope, HelloBlock ) 
 
   }
 
+  // Dummy Data
+  var defaultTxHash = "6f9e9570881e781db8c137c84c111a138e4a022e6b2def5e2a1589a802fe25f3"
+  HelloBlock.Transactions.get( {
+    tx_hash: defaultTxHash
+  }, function( res ) {
+    $scope.transaction = res.data.transaction
+  } )
+
 } )
