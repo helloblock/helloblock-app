68581ef538c34db9896a6c79f2b862ffe707d4ee
Scott Li <scottli0101@gmail.com>
2014-03-21 21:11:08 +1100

    fixed tx search bug

--- app/assets/javascripts/hbApp.js
+++ app/assets/javascripts/hbApp.js
@@ -198,37 +198,40 @@ hbApp.config(function($routeProvider) {
 
 })
 
-var QueryValidator = {
-  hexRegex: /^[0-9a-f]+$/i,
-  address: function(query) {
-    return cryptocoin.Address.validate(query)
-  },
-  transaction: function(query) {
-    if (query[0] === "0") {
-      return false
-    }
+var QueryValidator = {};
 
-    if (query.length !== 64) {
-      return false
-    }
+QueryValidator.hexRegex = /^[0-9a-f]+$/i;
 
-    if (query.match(this.hexRegex) === null) {
-      return false
-    }
+QueryValidator.address = function(query) {
+  return cryptocoin.Address.validate(query)
+}
 
-    return true
-  },
-  block: function(query) {
-    if (query.match(/^\d+$/) && parseInt(query) > 0) {
-      return true
-    }
-
-    if (query.match(this.hexRegex) && query.length === 64) {
-      return true
-    }
-
+QueryValidator.transaction = function(query) {
+  if (query.slice(0, 7) === "00000000") {
     return false
   }
+
+  if (query.length !== 64) {
+    return false
+  }
+
+  if (query.match(QueryValidator.hexRegex) === null) {
+    return false
+  }
+
+  return true
+}
+
+QueryValidator.block = function(query) {
+  if (query.match(/^\d+$/) && parseInt(query) > 0) {
+    return true
+  }
+
+  if (query.match(QueryValidator.hexRegex) && query.length === 64) {
+    return true
+  }
+
+  return false
 }
 
 var Route = {
